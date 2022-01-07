b9b29a25166397048ba5acf6bb9307527406757f
Noah Roberts <noahmroberts@gmail.com>
2022-01-07 13:31:23 -0600

    added express structure and started API call

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.post('/', (req, res) => {
+    const { beerStyle } = req.body;
+    const apiUrl = `https://api.punkapi.com/v2/beers/?beer_name=${beerStyle}`;
+
+    fetch(urlApi)
+        .then(res => res.json)
+        .then
+
+})
+
 app.listen(8000, () => {
-    console.log('Server listening @ port 8000')
+    console.log('Server listening at http://localhost:8000')
 })
